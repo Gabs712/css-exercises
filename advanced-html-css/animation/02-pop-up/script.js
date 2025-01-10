91c84a6b5c90778d38fde2db313461d5d5e69a51
gabs <gabs712.g@gmail.com>
2025-01-09 22:03:41 -0300

    Complete pop-up

--- advanced-html-css/animation/02-pop-up/script.js
+++ advanced-html-css/animation/02-pop-up/script.js
@@ -1,12 +1,22 @@
-const openButton = document.getElementById('trigger-modal');
-const closeButton = document.getElementById('close-modal');
+const openButton = document.getElementById("trigger-modal");
+const closeButton = document.getElementById("close-modal");
+const modalDiv = document.querySelector(".popup-modal");
+const backdrop = document.querySelector(".backdrop");
 
 function toggleModal() {
-  const modalDiv = document.querySelector('.popup-modal');
-  const backdrop = document.querySelector('.backdrop');
-  modalDiv.classList.toggle('show');
-  backdrop.classList.toggle('show');
+  modalDiv.classList.toggle("show");
+  backdrop.classList.toggle("show");
 }
 
-openButton.addEventListener('click', toggleModal);
-closeButton.addEventListener('click', toggleModal);
+openButton.addEventListener("click", () => {
+  toggleModal();
+  modalDiv.classList.remove("slide-out");
+  void modalDiv.offsetWidth;
+  modalDiv.classList.add("slide-in");
+});
+closeButton.addEventListener("click", () => {
+  toggleModal();
+  modalDiv.classList.remove("slide-in");
+  void modalDiv.offsetWidth;
+  modalDiv.classList.add("slide-out");
+});
